7bf38c7c1d0ef275d04ed4f302bc3d0d7398ad29
Ivan <irodrp00@estudiantes.unileon.es>
2026-01-09 23:25:39 +0000

    Added llm_pkg; created llm_chat_node

---
+++ ros2_ws/src/llm_pkg/llm_pkg/llm_chat_node.py
@@ -0,0 +1,54 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+import ollama
+
+
+class OllamaChat(Node):
+
+    def __init__(self):
+        super().__init__('ollama_chat')
+
+        self.request_sub = self.create_subscription(
+            String,
+            'input_request',
+            self.request_callback,
+            10
+        )
+
+        self.response_pub = self.create_publisher(
+            String,
+            'response',
+            10
+        )
+
+        self.get_logger().info(f"Ollama chat node is running!")
+
+    def request_callback(self, msg):
+
+        self.get_logger().info(f"Received request: {msg.data}")
+
+        response = ollama.chat(
+            model='llama3.2',
+            messages=[{
+                'role': 'user',
+                'content': msg.data,
+            }]
+        )
+
+        self.get_logger().info(f"LLM answers:\n  {response}")
+
+        output = String()
+        output.data = response['message']['content']
+
+        self.response_pub.publish(output)
+
+
+def main(args=None):
+
+    rclpy.init(args=args)
+    node = OllamaChat()
+    rclpy.spin(node)
+
+if __name__ == '__main__':
+    main()
